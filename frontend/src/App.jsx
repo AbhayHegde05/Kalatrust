997e93ee8900be44e97bc619ab136d3590f55ba2
AbhayHegde05 <abhayhegde643@gmail.com>
2025-10-03 09:50:22 +0530

    seventh final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; // Import the provider
+import { AuthProvider } from './context/AuthContext.jsx'; // Import the provider
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Home from './pages/Home.jsx';
