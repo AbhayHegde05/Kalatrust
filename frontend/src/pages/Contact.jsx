99962a5073bae365c32ec2da31c2bd655dcb5f3a
AbhayHegde05 <abhayhegde643@gmail.com>
2025-10-03 10:14:48 +0530

    11th final

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -9,12 +9,12 @@ const Contact = () => {
         <div className="contact-card">
           <h3>ಪ್ರಸನ್ನ ಪರಮೇಶ್ವರ ಹೆಗಡೆ</h3>
           <p>ಅಧ್ಯಕ್ಷರು</p>
-          <p>Phone: <a href="tel:+919999999999">+91 99999 99999</a></p>
+          <p>Phone: <a href="tel:+919480746537">+91 94807 46537</a></p>
         </div>
         <div className="contact-card">
           <h3>ಗೌತಮ್ ಆರ್ ಹೆಗಡೆ</h3>
           <p>ಕಾರ್ಯದರ್ಶಿ</p>
-          <p>Phone: <a href="tel:+918888888888">+91 88888 88888</a></p>
+          <p>Phone: <a href="tel:+919481284793">+91 94812 84793</a></p>
         </div>
       </div>
     </div>
